4d5e68e4b7fe2f9e20ea9c9da84b8cf7d766ef0e
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-07-08 17:52:55 +0200

    Add entity translations

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pytest --cov=custom_components/yamaha_ynca tests/ --cov-report term-missing --cov-report html
-mypy custom_components  --check-untyped-defs --disable-error-code typeddict-item
+mypy custom_components  --check-untyped-defs
