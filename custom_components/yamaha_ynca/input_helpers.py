77cbaf0d9f94c436014b0ae038b80b42c26da2e1
Philipp Hartl <philipp.hartl@informatik.uni-regensburg.de>
2024-10-13 14:26:24 +0200

    Changed the list of inputs into alphabetical order

--- custom_components/yamaha_ynca/input_helpers.py
+++ custom_components/yamaha_ynca/input_helpers.py
@@ -54,13 +54,13 @@ input_mappings: List[Mapping] = [
     Mapping(ynca.Input.HDMI5, []),
     Mapping(ynca.Input.HDMI6, []),
     Mapping(ynca.Input.HDMI7, []),
+    Mapping(ynca.Input.MULTICH, []),
     Mapping(ynca.Input.OPTICAL1, []),
     Mapping(ynca.Input.OPTICAL2, []),
-    Mapping(ynca.Input.MULTICH, []),
     Mapping(ynca.Input.PHONO, []),
     Mapping(ynca.Input.TV, []),
-    Mapping(ynca.Input.VAUX, []),
     Mapping(ynca.Input.USB, []),
+    Mapping(ynca.Input.VAUX, []),
 ]
 
 
