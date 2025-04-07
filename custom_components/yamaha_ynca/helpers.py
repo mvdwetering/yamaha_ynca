bd70d1cbe4494164eb6caaeaa9e69109651afd63
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2025-04-07 21:26:23 +0200

    Cleanup helpers

--- custom_components/yamaha_ynca/helpers.py
+++ custom_components/yamaha_ynca/helpers.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from homeassistant.helpers.entity import DeviceInfo, EntityDescription
 
@@ -21,7 +21,12 @@ class DomainEntryData:
     initialization_events: list[str]
 
 
-def scale(input_value, input_range, output_range):
+def scale(
+    input_value: float,
+    input_range: tuple[float, float],
+    output_range: tuple[float, float],
+) -> float:
+    """Scale a value from one range to another."""
     input_min = input_range[0]
     input_max = input_range[1]
     input_spread = input_max - input_min
@@ -35,7 +40,7 @@ def scale(input_value, input_range, output_range):
     return output_min + (value_scaled * output_spread)
 
 
-def receiver_requires_audio_input_workaround(modelname) -> bool:
+def receiver_requires_audio_input_workaround(modelname: str) -> bool:
     # These models do not report the (single) AUDIO input properly
     # Reported for RX-V475, including RX-V575, HTR-4066, HTR-5066 because they share firmware
     # See https://github.com/mvdwetering/yamaha_ynca/issues/230
@@ -59,6 +64,7 @@ def subunit_supports_entitydescription_key(entity_description, subunit) -> bool:
 
 class YamahaYncaSettingEntity:
     """Common code for YamahaYnca settings entities.
+
     Entities derived from this also need to derive from the standard HA entities.
     """
 
@@ -66,11 +72,11 @@ class YamahaYncaSettingEntity:
 
     def __init__(
         self,
-        receiver_unique_id,
+        receiver_unique_id: str,
         subunit: SubunitBase,
         description: EntityDescription,
         associated_zone: ZoneBase | None = None,
-    ):
+    ) -> None:
         self.entity_description = description
         self._subunit = subunit
 
@@ -98,16 +104,16 @@ class YamahaYncaSettingEntity:
             f"{self._receiver_unique_id_subunit_id}_{self.entity_description.key}"
         )
 
-    def update_callback(self, function, value):
+    def update_callback(self, function: str, _value: Any) -> None:
         if function in self._relevant_updates:
             self.schedule_update_ha_state()  # type: ignore
 
-    async def async_added_to_hass(self):
+    async def async_added_to_hass(self) -> None:
         self._subunit.register_update_callback(self.update_callback)
 
-    async def async_will_remove_from_hass(self):
+    async def async_will_remove_from_hass(self) -> None:
         self._subunit.unregister_update_callback(self.update_callback)
 
     @property
-    def available(self):
+    def available(self) -> bool:
         return self._associated_zone.pwr is ynca.Pwr.ON
