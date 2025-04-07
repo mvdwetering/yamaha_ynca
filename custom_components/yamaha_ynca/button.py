3a93ee6c2013ecc3f385519e7772aeabefe18d98
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2025-04-07 21:17:28 +0200

    Cleanup button

--- custom_components/yamaha_ynca/button.py
+++ custom_components/yamaha_ynca/button.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
-from homeassistant.components.button import ButtonEntity
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from typing import TYPE_CHECKING, Any
 
-from . import YamahaYncaConfigEntry
+from homeassistant.components.button import ButtonEntity
+from homeassistant.helpers.entity import DeviceInfo
+
 from .const import (
     CONF_NUMBER_OF_SCENES,
     DOMAIN,
@@ -14,12 +13,20 @@ from .const import (
     ZONE_ATTRIBUTE_NAMES,
 )
 
+if TYPE_CHECKING:
+    from homeassistant.core import HomeAssistant
+    from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+    import ynca
+
+    from . import YamahaYncaConfigEntry
+
 
 async def async_setup_entry(
-    hass: HomeAssistant,
+    _hass: HomeAssistant,
     config_entry: YamahaYncaConfigEntry,
     async_add_entities: AddEntitiesCallback,
-):
+) -> None:
     domain_entry_data = config_entry.runtime_data
     entities = []
     for zone_attr_name in ZONE_ATTRIBUTE_NAMES:
@@ -33,10 +40,11 @@ async def async_setup_entry(
                     if getattr(zone_subunit, f"scene{scene_id}name"):
                         number_of_scenes += 1
             number_of_scenes = min(MAX_NUMBER_OF_SCENES, number_of_scenes)
-            for scene_id in range(1, number_of_scenes + 1):
-                entities.append(
-                    YamahaYncaSceneButton(config_entry.entry_id, zone_subunit, scene_id)
-                )
+
+            entities.extend(
+                YamahaYncaSceneButton(config_entry.entry_id, zone_subunit, scene_id)
+                for scene_id in range(1, number_of_scenes + 1)
+            )
 
     async_add_entities(entities)
 
@@ -46,7 +54,9 @@ class YamahaYncaSceneButton(ButtonEntity):
 
     _attr_has_entity_name = True
 
-    def __init__(self, receiver_unique_id, zone, scene_id):
+    def __init__(
+        self, receiver_unique_id: str, zone: ynca.subunits.zone.ZoneBase, scene_id: int
+    ) -> None:
         self._zone = zone
         self._scene_id = scene_id
         self._update_functioname = f"SCENE{scene_id}NAME"
@@ -58,18 +68,18 @@ class YamahaYncaSceneButton(ButtonEntity):
             identifiers={(DOMAIN, f"{receiver_unique_id}_{self._zone.id}")}
         )
 
-    def update_callback(self, function, value):
+    def update_callback(self, function: str, _value: Any) -> None:
         if function == self._update_functioname:
             self.schedule_update_ha_state()
 
-    async def async_added_to_hass(self):
+    async def async_added_to_hass(self) -> None:
         self._zone.register_update_callback(self.update_callback)
 
-    async def async_will_remove_from_hass(self):
+    async def async_will_remove_from_hass(self) -> None:
         self._zone.unregister_update_callback(self.update_callback)
 
     @property
-    def name(self):
+    def name(self) -> str:
         return (
             getattr(self._zone, f"scene{self._scene_id}name")
             or f"Scene {self._scene_id}"
