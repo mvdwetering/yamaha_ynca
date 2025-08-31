fcdc8d8cfd9a677183ed412518d98626def5afa2
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2025-08-31 21:15:30 +0200

    Simplify setting of default serial_url value

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -190,16 +190,7 @@ async def test_unhandled_exception(hass: HomeAssistant) -> None:
 
 
 async def test_reconfigure_serial(hass: HomeAssistant, mock_ynca) -> None:
-    integration = await setup_integration(hass, mock_ynca)
-
-    # Make sure existing data is different from what we are changing it to
-    hass.config_entries.async_update_entry(
-        integration.entry,
-        data={
-            **integration.entry.data,
-            yamaha_ynca.const.CONF_SERIAL_URL: "old_serial_port",
-        },
-    )
+    integration = await setup_integration(hass, mock_ynca, serial_url="old_serial_port")
 
     # Flow goes to menu with connection options
     result = await hass.config_entries.flow.async_init(
@@ -250,15 +241,8 @@ async def test_reconfigure_serial(hass: HomeAssistant, mock_ynca) -> None:
 
 
 async def test_reconfigure_network(hass: HomeAssistant, mock_ynca) -> None:
-    integration = await setup_integration(hass, mock_ynca)
-
-    # Make sure existing data is different from what we are changing it to
-    hass.config_entries.async_update_entry(
-        integration.entry,
-        data={
-            **integration.entry.data,
-            yamaha_ynca.const.CONF_SERIAL_URL: "socket://old_hostname_or_ipaddress:12345",
-        },
+    integration = await setup_integration(
+        hass, mock_ynca, serial_url="socket://old_hostname_or_ipaddress:12345"
     )
 
     # Flow goes to menu with connection options
